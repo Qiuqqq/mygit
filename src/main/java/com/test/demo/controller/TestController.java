aa4f994507a46335ccdef3342e54c1714dc8a5d0
qs <qiushuang@one-sports.com>
2020-10-23 18:23:11 +0800

    first commit

---
+++ src/main/java/com/test/demo/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.test.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author ：qs
+ * @date ：2020/10/23 18:11
+ * @description：
+ */
+@RestController
+@RequestMapping("test")
+public class TestController {
+    @RequestMapping("/first")
+    public String test(){
+        return "first jenkins";
+    }
+}
